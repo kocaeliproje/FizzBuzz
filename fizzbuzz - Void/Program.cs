ca6e9575609463b76c5d5e8be192bfab85d4643a
Mehmet Aydın <54308525+kocaeliproje@users.noreply.github.com>
2022-09-24 23:38:51 +0300

    Add files via upload

---
+++ fizzbuzz - Void/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FizzzB{
+using System;
+class MainClass 
+ {
+   static void FizzBuzz(int num) 
+  {
+     // code goes here 
+    for (int i=1; i<num+1; i++ )
+    {
+       if (i%3==0) 
+       {
+         Console.Write("Fizz");
+       }
+      if (i%5==0)
+       {
+       Console.Write("Buzz") ;
+       }
+       if (i%3!=0&&i%5!=0&&i%15!=0)
+       {
+         Console.Write(i);
+       }
+       Console.Write(" ");
+     }
+  } 
+  static void Main() {  
+    // keep this function call here
+    int num=Convert.ToInt32(Console.ReadLine());
+    FizzBuzz(num);
+    } 
+  }
+ }
---
+++ fizzbuzz - static/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FizzzB{
+using System;
+class MainClass 
+{
+  public static string FizzBuzz(int num) {
+    // code goes here 
+    for (int i=1; i<num+1; i++ )
+    {
+       if (i%3==0) 
+       {
+         Console.Write("Fizz");
+       }
+      if (i%5==0)
+       {
+       Console.Write("Buzz") ;
+       }
+         if (i%3!=0&&i%5!=0&&i%15!=0)
+       {
+         Console.Write(i);
+       }
+       Console.Write(" ");
+    }
+    return (Convert.ToString(num));
+  }
+  static void Main() {  
+    // keep this function call here
+    int num=(Convert.ToInt32(Console.ReadLine()));
+    FizzBuzz(num);
+  } 
+
+}
+}
